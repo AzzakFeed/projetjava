9fa70909779db0d06a0757d79ffe33c27705f4b7
AzzakFeed <oshiro.kn@gmail.com>
2016-12-08 16:49:47 +0100

    Update Joueur.java

--- Joueur.java
+++ Joueur.java
@@ -3,7 +3,8 @@ public class Joueur{
 	//Champs
 	private String couleur; // Indique la couleur du joueur. Sert de référentiel.
 	private int nConditionDeVictoire; //Le numéro de la condition de victoire que le joueur devra accomplir pour gagner la partie.
-	
+	private static int n = 0; //Nombre de joueurs
+	private int id; //id 
 	
 	//Constructeur vide
 	public Joueur () {
@@ -15,6 +16,7 @@ public class Joueur{
 	public Joueur (String c, int n){
 		couleur = c;
 		nConditionDeVictoire = n;
+		id = ++n; // Incrémentation à chaque création d'une instance.
 	}
 	
 	//set
