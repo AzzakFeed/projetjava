b9df01986bd22bed0c3d3db179fc5ff79cb74a3d
AzzakFeed <oshiro.kn@gmail.com>
2016-12-09 14:58:09 +0100

    Add files via upload

---
+++ groupe.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+<title>Page Title</title>
+</head>
+<body>
+
+<div class="top"> 
+<h2> Groupe de musique</h2>
+</div>
+
+<div class="left">
+    LEFT
+</div>
+<div class="main">
+ <button type="button">Je suis musicien et je recherche un groupe</button> 
+ 
+ 
+
+ </div>
+
+
+</body>
+</html>
