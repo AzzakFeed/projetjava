e79c76e6ae45fb298c1b7899b2c4650d08e97bb9
AzzakFeed <oshiro.kn@gmail.com>
2016-12-13 11:24:34 +0100

    Add files via upload

---
+++ tirageconditions.java
@@ -0,0 +1,12 @@
+//Tirage au sort des conditions de victoire
+
+// Total 8
+public class Tirage{
+private float tirage;
+private int i;
+
+for (int i=0; j<3; i++){
+tirage = 1+(int)(Math.random()* ((8-1) +1)))   //
+System.out.println(tirage);
+
+}
