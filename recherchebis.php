39d721f1c2419ddd7fc600a71d0f50c2d11e7690
Diodiot <vincent.cadiot@etu.univ-nantes.fr>
2017-01-12 14:57:26 +0100

    Add files via upload

--- recherchebis.php
+++ recherchebis.php
@@ -26,30 +26,86 @@
         </div>
     </header>
 
-<div class="contenu" id="contenuform"
+<div class="contenu" id="contenuform">
+
 <?php
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 $mysqli = new mysqli("mysql", "E102952E","WKXM9C9F", "E102952E");
 
 if (isset( $_POST['nomgroupe'])){
- $a = $_POST['nomgroupe'];
+ $nomg = $_POST['nomgroupe'];
  }
 
 if (isset( $_POST['villegroupe'])){
- $b = $_POST['villegroupe'];
+ $villeg = $_POST['villegroupe'];
 }
-if (isset( $_POST['nomgroupe'])){
-	$req = "SELECT nomg FROM Groupe WHERE nomg='$a'";
-	$resultat=mysqli_query($mysqli,$req,MYSQLI_STORE_RESULT);
-	while($tab=mysqli_fetch_assoc($resultat)){
-		echo "Nom: " . $tab["nomg"]. "<br>";
+
+if (isset($_POST['guitare']) || isset($_POST['basse']) || isset($_POST['batterie'])|| isset($_POST['chanteur']) ){
+for($i=1;$i<=4;$i++){
+	if(isset($_POST['guitare'])){
+		$instru='guitare';
+	}else{
+		if(isset($_POST['basse'])){
+			$instru='basse';
+		}else{
+			if(isset($_POST['batterie'])){
+				$instru='batterie';
+			}else{
+				if(isset($_POST['chanteur'])){
+					$instru='chanteur';
+				}
+			}
+		}
 	}
 }
+}
+
+if (isset($_POST['rock']) || isset($_POST['blues']) || isset($_POST['jazz'])|| isset($_POST['rap']) ) {
+for($j=1;$j<=4;$j++){
+	if(isset($_POST['rock'])){
+		$stylem='Rock';
+	}else{
+		if(isset($_POST['blues'])){
+			$stylem='Blues';
+		}else{
+			if(isset($_POST['jazz'])){
+				$stylem='Jazz';
+			}else{
+				if(isset($_POST['rap'])){
+					$stylem='Rap';
+				}
+			}
+		}
+	}
+}
+}
+//rajouter conditions négatives
+if (isset( $_POST['nomgroupe'])){
+	$req = "SELECT nomg, villeg, styleg, telephoneg FROM Groupe WHERE nomg='$nomg'";
+	$stmt = mysqli_prepare($mysqli,$req);
+	mysqli_stmt_execute($stmt);
+	mysqli_stmt_bind_result($stmt, $nomg, $villeg, $styleg, $telephoneg);
+	while (mysqli_stmt_fetch($stmt)) {
+		printf ("Nom du groupe: %s <br> Ville: %s <br> Style: %s <br> Téléphone: %s <br> __________________________________ <br>",$nomg,$villeg,$styleg,$telephoneg);
+	}
+	mysqli_stmt_close($stmt);
+}
+
+if (isset( $_POST['villegroupe'])){
+	$req = "SELECT nomg, villeg, styleg, telephoneg FROM Groupe WHERE villeg='$villeg'";
+	$stmt = mysqli_prepare($mysqli,$req);
+	mysqli_stmt_execute($stmt);
+	mysqli_stmt_bind_result($stmt, $nomg, $villeg, $styleg, $telephoneg);
+	while (mysqli_stmt_fetch($stmt)) {
+		printf ("Nom du groupe: %s <br> Ville: %s <br> Style: %s <br> Téléphone: %s <br> __________________________________ <br>",$nomg,$villeg,$styleg,$telephoneg);
+	}
+	mysqli_stmt_close($stmt);
+}
+
 $mysqli->close();
 
 ?>
-
 
 
 </div>
